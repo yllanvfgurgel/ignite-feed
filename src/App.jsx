75722b8bd6c338cdcac9f586c2b2f4d6c9fd7860
Yllan Gurgel <the_yllan@hotmail.com>
2023-03-06 21:45:33 -0300

    primeiro commit e header da aplicação

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Header } from "./components/Header"
+
+import styles from './App.module.css'
+
+import './global.css'
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className="wrapper">
+
+      </div>
+    </div>
+  )
+}
+
+export { App }
