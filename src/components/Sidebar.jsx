1a7248778675b617bff8566a567475c2e258db97
Yllan Gurgel <the_yllan@hotmail.com>
2023-03-07 21:29:18 -0300

    finalizando estilização da sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -8,12 +10,16 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=50" 
             />
             <div className={styles.profile}>
+                <img src="https://github.com/yllanvfgurgel.png" />
                 <strong>Yllan Gurgel</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
-                <a href="#">Editar seu perfil</a>
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>
             </footer>
         </aside>
     );
